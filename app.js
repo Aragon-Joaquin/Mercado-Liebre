aaf0a5b224d61e5bdc86da05b27156aca4c1a29d
unknown <aragonjoaquin05@gmail.com>
2023-07-22 20:06:36 -0300

    directories changes

--- app.js
+++ app.js
@@ -9,9 +9,21 @@ const puerto = 3001;
 app.use(express.json());
 app.use('/', express.static(__dirname + '/public'));
 
+//HOME & SUBDOMAINS
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('views/home.html') );
 })
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('views/login.html') );
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve('views/register.html') );
+})
+
+// LISTEN ON PORT
 
 app.listen(puerto, () => {
     console.log(`Aplicación esuchando en http://localhost:${puerto}`);
